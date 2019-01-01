e6b91394dfeecd09ab752e0b322403ffc71e087f
Ankeru <tavonaar@gmail.com>
2019-01-01 18:56:05 +0300

    *

---
+++ htmlDownloading/url.py
@@ -0,0 +1,14 @@
+import urllib.request
+urls = open("C://Users//Kamio//.git//pyTasks//htmlDownloading//url.txt", "r")
+for url in urls.readlines():
+    if url.strip() != '':
+        htmlCodeOfPage = urllib.request.urlopen(url)
+        print(url)
+        fileToWrite = open("C://Users//Kamio//.git//pyTasks//htmlDownloading///htmls/" + str(url).strip()[8:] + '.html', "w")
+        for line in htmlCodeOfPage.readlines():
+            fileToWrite.write(str(line))
+            fileToWrite.write('/n')
+        fileToWrite.close()
+
+            
+        
