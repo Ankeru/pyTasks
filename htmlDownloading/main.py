109e92571fb8a94129c79ba72bedfe70fb2ad33e
Ankeru <tavonaar@gmail.com>
2019-01-01 18:58:33 +0300

    Name of file changed

--- htmlDownloading/main.py
+++ htmlDownloading/main.py
@@ -7,7 +7,6 @@ for url in urls.readlines():
         fileToWrite = open("C://Users//Kamio//.git//pyTasks//htmlDownloading///htmls/" + str(url).strip()[8:] + '.html', "w")
         for line in htmlCodeOfPage.readlines():
             fileToWrite.write(str(line))
-            fileToWrite.write('/n')
         fileToWrite.close()
 
             
